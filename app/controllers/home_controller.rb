55d80fb83a5c006abea444188aff65016147bcfa
Daniel Hum <dan@oneuphero.com>
2011-08-20 04:21:04 +0800

    use title + hashtag in tweet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
     @item = Item.skip(offset).first
     price_offset = rand(@item.prices.count)
     @price = @item.prices.skip(price_offset).first
+    dollars = @price.value / 100
+    cents = @price.value - (dollars * 100)
+    @price_value = '$' + dollars.to_s + '.' + cents.to_s.rjust(2,'0')
 
     if @item.prices.order_by([:value, :asc]).first.value == @price.value
       @cheapest = true
@@ -12,6 +15,8 @@ class HomeController < ApplicationController
       @cheapest = false
       @expensive = true
     end
+
+    @title = "One #{@item.name} costs #{@price_value} at #{@price.location}"
   end
 
 end
