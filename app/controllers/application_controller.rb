6946060c96fccec8184b48629e0b655df6f8ca36
Daniel Hum <dan@oneuphero.com>
2011-08-19 19:23:57 +0800

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'optimus' && password == 'zunuP&q9'
+    end
+  end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_filter :authenticate, :except => [:show]
   # GET /items
   # GET /items.json
   def index
