9daa29430583263e9df645e0181691d1140002c7
Daniel Hum <dan@oneuphero.com>
2011-08-19 18:23:20 +0800

    configured for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onehowmuch::Application.config.secret_token = '85bc44f2c8e0044475a6b0881b59a8672c2d129f1d7b06786f9215591ad650e20502df28ad055784e1d85fbb40067ea300c84b11157daa9104d7de6cced3aeb7'
