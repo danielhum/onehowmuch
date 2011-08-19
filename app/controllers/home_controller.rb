f80ff90cbdd3bda068b2fe757bfc4bc81936bd49
Daniel Hum <dan@oneuphero.com>
2011-08-20 03:20:10 +0800

    added quip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,14 @@ class HomeController < ApplicationController
     @item = Item.skip(offset).first
     price_offset = rand(@item.prices.count)
     @price = @item.prices.skip(price_offset).first
+
+    if @item.prices.order_by([:value, :asc]).first.value == @price.value
+      @cheapest = true
+      @expensive = false
+    elsif @item.prices.order_by([:value, :asc]).last.value == @price.value
+      @cheapest = false
+      @expensive = true
+    end
   end
 
 end
